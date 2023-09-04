6c042579da7e806d434719a7417194eae354e66a
paoluisianagarcia <143976833+paoluisianagarcia@users.noreply.github.com>
2023-09-04 16:43:57 -0600

    Consulta de notas por fecha y folio
    Codigo para la consulta de notas y reportes tabulados.

--- opciones_del_menu.py
+++ opciones_del_menu.py
@@ -132,3 +132,118 @@ class RegistrarNota:
                 return False
             else:
                 print('Opción no válida. Intente de nuevo')
+class ConsultasYReportes:
+    def __init__(self, notas_registradas):
+        self.notas_registradas = notas_registradas
+        
+        while True:
+            os.system('cls')
+            self.menu_consultas_y_reportes()
+
+            match self.eleccion_consulta:
+                case 1:
+                    self.consulta_por_periodo()
+                case 2:
+                    self.consulta_por_folio()
+                case 3:
+                    break
+
+    def menu_consultas_y_reportes(self):
+        print('''
+[#9999FF]CONSULTAS Y REPORTES[/#9999FF]
+              
+[#7AFFFF]--Menú Consultas--[/#7AFFFF]
+
+1 - Consulta por período
+2 - Consulta por folio
+3 - Regresar al menú principañ
+              
+''')
+        while True:
+            try:
+                self.eleccion_consulta = int(input('Elija una opción (indicando su respectivo número): '))
+            except ValueError:
+                print('Opción no válida. Intente de nuevo')
+            else:
+                if self.eleccion_consulta > 0 and self.eleccion_consulta <= 3:
+                    break
+                else:
+                    print('Opción no válida. Intente de nuevo')
+        
+                    
+    def consulta_por_periodo(self):
+
+        os.system('cls')
+
+        print('[#7AFFFF]--Consulta por periodo--[#/7AFFFF]\n')
+        
+        while True:
+            try:
+                self.respuesta = input('Ingrese su fecha inicial (dd/mm/aaaa): ')
+                self.fecha_inicial = datetime.datetime.strptime(self.respuesta,"%d/%m/%Y").date()
+        
+                self.respuesta = input('Ingrese su fecha final (dd/mm/aaaa): ')
+                self.fecha_final = datetime.datetime.strptime(self.respuesta,"%d/%m/%Y").date()
+            except ValueError:
+                print('Tipo de formato no válido. Intente de nuevo\n')
+            except Exception as error:
+                print(f'Ocurrió un problema: {error}\n')
+            else:
+                break
+
+        notas_del_periodo = Table(title="--Notas dentro del perído--", style="#7AFFFF")
+        notas_del_periodo.add_column("Folio", justify="left", style="#9999FF")
+        notas_del_periodo.add_column("Fecha de la nota", justify="left", style="white")
+
+        contador = 0
+        for folio, datos in self.notas_registradas.items():
+            fecha = datos[0]
+            if fecha >= self.fecha_inicial and fecha <= self.fecha_final:
+                notas_del_periodo.add_row(f'{folio}', f'{fecha}')
+                contador += 1
+                
+        if contador == 0:
+            print('\n[red]No existen notas dentro de este período[/red]\n')
+
+        print(notas_del_periodo)
+        input()
+    
+    def consulta_por_folio(self):
+
+        print('\n([#9999FF]En número entero, con máximo 4 carácteres)[#/9999FF]')
+        
+        while True:
+            try:
+                folio_consutado = int(input('Folio a Consultar: '))
+            except ValueError:
+                print('Ese no es un número válido. Intenta de nuevo\n')
+            except Exception as error:
+                print('Ocurrió un problema. Intente de nuevo')
+            else:
+                if len(str(folio_consutado)) == 4:
+                    break
+                else:
+                    print('Tienen que ser sólo 4 carácteres\n')
+
+        datos_consultados = self.notas_registradas[folio_consutado]
+
+        os.system('cls')
+
+        print('[#7AFFFF]--Consulta por folio--[#/7AFFFF]\n')
+
+        os.system('cls')
+        
+        nota_consultada = Table(title=f'--Nota consultada--')
+
+        nota_consultada.add_column("Detalles", justify="left", style="#9999FF")
+        nota_consultada.add_column("Datos", justify="left", style="white")
+
+        nota_consultada.add_row('Folio', f'{folio_consutado}')
+        nota_consultada.add_row('Fecha', f'{datos_consultados[0]}')
+        nota_consultada.add_row('Nombre del cliente', f'{datos_consultados[1]}')
+        nota_consultada.add_row('Monto a pagar', f'{datos_consultados[2]}')
+        nota_consultada.add_row('Detalle de nota', f'{datos_consultados[3]}')
+
+        print(nota_consultada)
+
+        input()
